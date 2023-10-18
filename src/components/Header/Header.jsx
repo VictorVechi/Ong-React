a6f12872f1e910232d5d64fb3accf6fbe8c65195
VictorVechi <victorhvechi@gmail.com>
2023-10-18 11:07:12 -0300

    criação do header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {StyleHeader} from './header.styles'
+
+const Header = () => {
+  return (
+        <StyleHeader>
+            <p>aa</p>
+        </StyleHeader>
+  )
+}
+
+export default Header
