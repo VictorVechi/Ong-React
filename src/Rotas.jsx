ffa6befa22deb55e166bea0401b18e346f1cd3cd
VictorVechi <victorhvechi@gmail.com>
2023-10-18 20:20:52 -0300

    adição do estilo global

--- src/Rotas.jsx
+++ src/Rotas.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import LandingPage from './pages/LandingPage'
+import { GlobalStyle } from './style/GlobalStyle.styles'
 
 const Rotas = () => {
   return (
     <BrowserRouter>
+        <GlobalStyle/>
         <Routes>
             <Route path='/' element={<LandingPage />}/>
         </Routes>
