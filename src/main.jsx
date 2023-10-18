2b25cbcf44fc2e0ae029353efa2dc05ff1a7f26f
VictorVechi <victorhvechi@gmail.com>
2023-10-18 11:07:44 -0300

    remoção de importação desnecessária

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.css'
-import LandingPage from './pages/LandingPage'
 import Rotas from './Rotas'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
