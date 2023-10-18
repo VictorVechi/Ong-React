9c854e3eada424a3642d410286fe7669daef9895
VictorVechi <victorhvechi@gmail.com>
2023-10-18 20:21:12 -0300

    criação de estilo global

---
+++ src/style/GlobalStyle.styles.js
@@ -0,0 +1,32 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+    * {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family:  'Roboto', sans-serif;
+}
+
+html,
+:root {
+  width: 100%;
+  min-height: 100vh;
+}
+
+button,
+a {
+  cursor: pointer;
+}
+
+a, li{
+  text-decoration: none;
+  list-style: none;
+  color: inherit;
+}
+
+button,
+input {
+  outline: none;
+}
+`
