935121021bf08b695e5ebe23d5bff163b0a03b79
VictorVechi <victorhvechi@gmail.com>
2023-10-18 10:22:56 -0300

    importação das rotas

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
+import LandingPage from './pages/LandingPage'
+import Rotas from './Rotas'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-
+    <Rotas/>
   </React.StrictMode>,
 )
