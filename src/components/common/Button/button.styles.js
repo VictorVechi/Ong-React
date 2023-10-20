be5f7f582bb6f8ebf688a633139daa157f5c48e1
VictorVechi <victorhvechi@gmail.com>
2023-10-20 15:41:47 -0300

    estilização do botão

--- src/components/common/Button/button.styles.js
+++ src/components/common/Button/button.styles.js
@@ -1,18 +1,16 @@
 import styled from "styled-components";
 
-
-
 export const StyleButton = styled.button`
     display: flex;
     padding: 10px 20px;
     align-items: center;
     border-radius: 8px;
-    /* background: #1d3557; */
+    background: #CCEEFF;
+    border: solid ;
     box-shadow: 5px 6px 4px 0px rgba(0, 0, 0, 0.25);
     /* color: #fff; */
     font-size: 24px;
     font-style: normal;
     font-weight: 700;
     line-height: normal;
-
 `
