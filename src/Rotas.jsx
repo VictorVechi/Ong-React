bdd68acc32b30e52b8078e3812d5c6c322c62372
VictorVechi <victorhvechi@gmail.com>
2023-10-19 22:28:00 -0300

    correção da rota para cadastro

--- src/Rotas.jsx
+++ src/Rotas.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import LandingPage from './pages/LandingPage'
 import { GlobalStyle } from './style/GlobalStyle.styles'
+import Cadastro from './pages/Cadastro/Cadastro'
 
 const Rotas = () => {
   return (
