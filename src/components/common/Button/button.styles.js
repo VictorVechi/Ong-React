46fa3383a5e539f55be232c89b483c6d546159b7
VictorVechi <victorhvechi@gmail.com>
2023-10-20 16:23:11 -0300

    cria o estilo padrão do botão

--- src/components/common/Button/button.styles.js
+++ src/components/common/Button/button.styles.js
@@ -5,10 +5,9 @@ export const StyleButton = styled.button`
     padding: 10px 20px;
     align-items: center;
     border-radius: 8px;
-    background: #CCEEFF;
+    background: ${(props)=>props.theme.blue};
     border: solid ;
     box-shadow: 5px 6px 4px 0px rgba(0, 0, 0, 0.25);
-    /* color: #fff; */
     font-size: 24px;
     font-style: normal;
     font-weight: 700;
