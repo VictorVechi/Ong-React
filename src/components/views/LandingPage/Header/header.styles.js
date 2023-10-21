57cdba9ace0e6bec44462ae9e44866722ccd47ae
VictorVechi <victorhvechi@gmail.com>
2023-10-20 21:00:54 -0300

    corrige estilo no header

--- src/components/views/LandingPage/Header/header.styles.js
+++ src/components/views/LandingPage/Header/header.styles.js
@@ -5,32 +5,46 @@ export const StyleHeader = styled.header`
     width: 100%;
     display: flex;
     justify-content: space-between;
-    padding: 10px;
+    padding: 25px;
+    align-items: baseline;
+    background-color: ${(props)=>props.theme.blue};
+    height: 12%;
 
-    .logo {
-        /* color: #457b9d; */
-        font-size: 32px;
-        font-style: normal;
-        font-weight: 700;
-        line-height: normal;
-    }
     .lista-Nav {
         width: fit-content;
         display: flex;
         align-items: center;
         gap: 24px;
-  
     }
 
     .item-lista {
-        /* color: #457b9d; */
         font-size: 24px;
         font-style: normal;
         font-weight: 400;
         line-height: normal;
+        padding: 5px 10px;
     }
 
     .item-lista:hover {
         opacity: 0.7;
+    }
+
+    .item-lista:nth-child(3){
+        border: 2px solid ${(props)=>props.theme.orangeDark} ;
+        border-radius: 5px;
+    }
+
+    .item-lista:nth-child(3):hover{
+        border-color: ${(props)=>props.theme.black};
+        color: ${(props)=>props.theme.darkBlue};
+    }
+
+    button {
+        background-color: ${(props)=>props.theme.orangeDark};
+    }
+
+    button:hover {
+        background-color: ${(props)=>props.theme.blue};
+        border-color: ${(props)=>props.theme.orangeDark};
     }
 `
