4e024ac255a7d19e349b0bef32459af3feab3daf
VictorVechi <victorhvechi@gmail.com>
2023-10-20 21:01:37 -0300

    adiciona o componente main

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import Header from '../../components/views/LandingPage/Header/Header'
+import Main from '../../components/views/LandingPage/Main/Main'
+import { StyleLP } from './landingPage.styles'
 
 const LandingPage = () => {
   return (
-    <div>
+    <StyleLP>
         <Header/>
-    </div>
+        <Main/>
+    </StyleLP>
   )
 }
 
