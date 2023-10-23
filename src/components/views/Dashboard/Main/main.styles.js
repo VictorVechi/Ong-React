a32b1722cf078e6144f9c8c15eb5137fbe6e0fc3
rffantin <rffantin@gmail.com>
2023-10-23 20:37:34 -0300

    feat: :sparkles: Criação da estilização para o componente MainDashboard

---
+++ src/components/views/Dashboard/Main/main.styles.js
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+
+
+export const StylesMainDash = styled.main`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 88%;
+    background-color: #FF960B8F;
+
+    section {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 50%;
+        height: 100%;
+    }  
+
+    .img-adote {
+        width: 80%;
+        height: 80%;
+    }
+
+    section:last-child{
+        flex-direction: row;
+        flex-wrap: wrap;
+        gap: 10px;
+    }
+`
