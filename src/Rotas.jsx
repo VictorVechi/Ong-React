5a08ee261b0a2dd49185a24365cd573565d48ff0
FrancheskaOliveira <francheska__@hotmail.com>
2023-10-24 14:28:05 -0300

    Atualiza Rotas e Header com o caminho da page QS

--- src/Rotas.jsx
+++ src/Rotas.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import LandingPage from './pages/LandingPage/LandingPage'
 import { GlobalStyle } from './style/GlobalStyle.styles'
+import QuemSomos from './pages/QuemSomos/QuemSomos'
 import Login from './pages/Login/login'
 import Cadastro from './pages/Cadastro/Cadastro'
 import { ThemeProvider } from 'styled-components'
@@ -14,6 +15,7 @@ const Rotas = () => {
       <ThemeProvider theme={theme}>
         <Routes>
         <Route path='/' element={<LandingPage />} />
+        <Route path='/QuemSomos' element={<QuemSomos />} />
         <Route path="/login" element={<Login />} />
         <Route path='/cadastro' element={<Cadastro />} /> 
       </Routes></ThemeProvider>
