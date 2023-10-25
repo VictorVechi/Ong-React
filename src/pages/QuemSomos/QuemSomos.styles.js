e092410f3ad0af0f4a2275f7e339a1166d86f487
FrancheskaOliveira <francheska__@hotmail.com>
2023-10-25 15:34:30 -0300

    atualizacao estilo da pagina QuemSomos

--- src/pages/QuemSomos/QuemSomos.styles.js
+++ src/pages/QuemSomos/QuemSomos.styles.js
@@ -16,17 +16,30 @@ export const StyleQuemSomos = styled.div`
         padding-left: 10vw;
         padding-right: 10vw;
     }
+
+    .fileira {
+        display: flex;
+        text-align: center;
+        justify-content: space-between;
+        padding-left: 10vw;
+        padding-right: 10vw;
+        padding-bottom: 3vw;
+    }
 `;
 
 export const CardContainerRow = styled.div`
     display: flex; 
     justify-content: space-between;
+    padding-top: 3vw;
     padding-left: 10vw;
     padding-right: 10vw;
 `;
 
 export const CardContainer = styled.div`
     border-radius: 50%;
+    border: 2px solid black;
+    box-shadow: 2px 2px 8px 2px rgba(0, 0, 0, 0.25);
+    background-color: ${(props)=>props.theme.blue};
     width: 200px;
     height: 200px;
     overflow: hidden;
