ef5dd2ac57c1b7edbd5d33426da962922920337e
VictorVechi <victorhvechi@gmail.com>
2023-10-25 10:05:08 -0300

    post de endereços e post de unidades

--- src/services/api.js
+++ src/services/api.js
@@ -5,15 +5,60 @@ const api = axios.create({
 });
 
 export const postUsuario = async (data, senha) => {
-    const config = {
-        headers: {
-            'password': senha
-        }
-    };
-
-    const response = await api.post('/usuarios', data, config);
-    return response.data;
+    try {
+        const config = {
+            headers: {
+                'password': senha
+            }
+        }; 
+        const response = await api.post('/usuarios', data, config);
+        return response.data;
+    } catch (error) {
+        console.log(error.message);
+    }
 };
+
+export const postEnderecoUsuario = async() => {
+    try {
+        const endereco = {
+            cep: " ",
+            rua: " ",
+            numero: " ",
+            complemento: " "
+        }
+    
+        const responseEndereco = api.post('/enderecoUsuario', endereco)
+        return responseEndereco
+    
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export const postEnderecoUnidade = async() => {
+    try {
+        const endereco = {
+            cep: " ",
+            rua: " ",
+            numero: " ",
+        }
+    
+        const responseEndereco = api.post('/enderecoUnidade', endereco)
+        return responseEndereco
+    
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export const postUnidade = async (data) => {
+    try {
+        const response = await api.post('/unidades', data)
+        return response.data
+    } catch (error) {
+        console.log(error.message);
+    }
+}
 
 export const loginUsuario = async (email, senha) => {
     try {
